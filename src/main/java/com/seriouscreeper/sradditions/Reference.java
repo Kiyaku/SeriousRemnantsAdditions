6a2380fd479490a6c4f6b3198091fc22f1dd9ca9
Sven <kiyaku.is@gmail.com>
2017-03-23 22:50:01 -0700

    increased version number

--- src/main/java/com/seriouscreeper/sradditions/Reference.java
+++ src/main/java/com/seriouscreeper/sradditions/Reference.java
@@ -4,7 +4,7 @@ package com.seriouscreeper.sradditions;
 public class Reference {
     public static final String MOD_ID = "sradditions";
     public static final String NAME = "SeriousCreeper's SR Additions";
-    public static final String VERSION = "0.1.2";
+    public static final String VERSION = "0.1.3";
     public static final String ACCEPTED_VERSIONS = "[1.11.2]";
 
     public static final String CLIENT_PROXY_CLASS = "com.seriouscreeper.sradditions.proxy.ClientProxy";
