5064e7be10ef63cc210362d8d14b9a3cb97c372e
Sven <kiyaku.is@gmail.com>
2017-04-08 03:55:42 -0700

    i meant 0.1.8, woops!

--- src/main/java/com/seriouscreeper/sradditions/Reference.java
+++ src/main/java/com/seriouscreeper/sradditions/Reference.java
@@ -4,7 +4,7 @@ package com.seriouscreeper.sradditions;
 public class Reference {
     public static final String MOD_ID = "sradditions";
     public static final String NAME = "SeriousCreeper's SR Additions";
-    public static final String VERSION = "0.1.7";
+    public static final String VERSION = "0.1.8";
     public static final String ACCEPTED_VERSIONS = "[1.11.2]";
 
     public static final String MODID_TAN = "toughasnails";
